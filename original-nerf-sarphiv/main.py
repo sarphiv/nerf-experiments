bbd56771e697cc175042eedc22c433d01777eb5d
Lauge Hermansen <76589717+LaugeHermansen@users.noreply.github.com>
2023-07-09 14:40:20 +0200

    none

--- original-nerf-sarphiv/main.py
+++ original-nerf-sarphiv/main.py
@@ -14,12 +14,20 @@ if __name__ == "__main__":
     # Set seeds
     pl.seed_everything(1337)
 
+    print("Starting training...")
+    # check if we have a GPU available
+    if th.cuda.is_available():
+        device = th.device("cuda")
+        print("Using GPU")
+    else:
+        device = th.device("cpu")
+        print("Using CPU")
 
     # Set up weights and biases logger
-    wandb_logger = WandbLogger(
-        project="nerf-experiments", 
-        entity="metrics_logger"
-    )
+    # wandb_logger = WandbLogger(
+    #     project="nerf-experiments", 
+    #     entity="metrics_logger"
+    # )
 
 
     # Set up data module
@@ -42,16 +50,17 @@ if __name__ == "__main__":
     trainer = pl.Trainer(
         accelerator="auto",
         max_epochs=256,
-        precision="16-mixed",
-        logger=wandb_logger,
+        precision="bf16",
+        # precision="16-mixed",
+        # logger=wandb_logger,
         callbacks=[
-            Log2dImageReconstruction(
-                wandb_logger=wandb_logger,
-                epoch_period=1,
-                validation_image_name="r_2",
-                batch_size=1024*4,
-                num_workers=8
-            ),
+            # Log2dImageReconstruction(
+                # wandb_logger=wandb_logger,
+                # epoch_period=1,
+                # validation_image_name="r_2",
+                # batch_size=1024*4,
+                # num_workers=8
+            # ),
             LearningRateMonitor(
                 logging_interval="epoch"
             )
@@ -63,7 +72,8 @@ if __name__ == "__main__":
     model = NerfOriginal(
         near_sphere_normalized=2,
         far_sphere_normalized=7,
-        samples_per_ray=192,
+        samples_per_ray_coarse=64,
+        samples_per_ray_fine=192,
         fourier_levels_pos=10,
         fourier_levels_dir=4,
         learning_rate=5e-4,
