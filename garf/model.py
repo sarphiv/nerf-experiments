5721d171a0951b5edd4b7e103a33541afe4fdc32
sarphiv <32750711+sarphiv@users.noreply.github.com>
2023-10-10 19:08:15 +0200

    NerfAcc, documentation, and mOre metrics

--- garf/model.py
+++ garf/model.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional, Dict
+from typing import Callable, Literal, Optional, Dict
 
 import torch as th
 import torch.nn as nn
@@ -61,14 +61,14 @@ class Garf(pl.LightningModule):
             gaussian_init_min=gaussian_init_min,
             gaussian_init_max=gaussian_init_max,
         )
-        
-        
+
         # Transmittance estimator
         self.transmittance_estimator = nerfacc.PropNetEstimator()
-        
-        
-        # Enable manual optimization
+
+
+        # Enable manual optimization because using multiple optimizers
         self.automatic_optimization = False
+
 
 
     def _get_positions(
@@ -94,7 +94,7 @@ class Garf(pl.LightningModule):
         #   t_starts and t_ends are reshaped to (n_rays, n_samples, 1) for broadcasting.
         #   The singleton dimensions in the tensors will then broadcast to (n_rays, n_samples, 3).
         return ray_origs[:, None] + ray_dirs[:, None] * ((t_starts + t_ends))[..., None] / 2
-        
+
 
     def _create_proposal_forward(
         self, 
@@ -183,40 +183,16 @@ class Garf(pl.LightningModule):
         Forward pass of the model.
         Given the ray origins and directions, compute the rgb values for the given rays.
 
-        Parameters:
-        -----------
+        Args:
             ray_origs: Tensor of shape (n_rays, 3) - focal point position in world, i.e. origin in camera coordinates
             ray_dirs: Tensor of shape (n_rays, 3) - direction vectors (unit vectors) of the viewing directions
 
         Returns:
-        --------
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            rgb: Tensor of shape (n_rays, 3) - the rgb values of the given rays
+            opacity: Tensor of shape (n_rays, 1) - the opacity values of the given rays
+            depth: Tensor of shape (n_rays, 1) - the depth values of the given rays
+            extras: Dict - extra intermediate calculation data, e.g. transmittance
         """
-        
-        
-        
-        
-        
-        # TODO: Finish documentation
-        
-        
-        
-        
-        
         # Estimate positions to sample
         t_starts, t_ends = self.transmittance_estimator.sampling(
             prop_sigma_fns=[self._create_proposal_forward(ray_origs, ray_dirs)], 
@@ -241,44 +217,65 @@ class Garf(pl.LightningModule):
         )
 
 
-        # Return colors for the given pixel coordinates (n_rays, 3)
+        # Return colors for the given pixel coordinates (n_rays, 3),
+        #  together with the opacity (n_rays, 1) and depth (n_rays, 1),
+        #  and the extras Dict
         return rgb, opacity, depth, extras
 
 
 
 
-    def _forward_loss(self, batch: DatasetOutput, batch_idx: int, stage: str):
+    def _forward_loss(self, batch: DatasetOutput, batch_idx: int, stage: Literal["train", "val", "test"]):
+        """Forward pass that also calculates and logs losses.
+        
+        Args:
+            batch (DatasetOutput): Batch of data
+            batch_idx (int): Index of the batch
+            stage (Literal["train", "val", "test"]): Stage of training
+        
+        Returns:
+            tuple[th.Tensor]: Ordered losses for each optimizer
+        """
+        # Decontsruct batch
         ray_origs, ray_dirs, ray_colors = batch
         
+        # Forward pass
         ray_colors_pred, ray_opacity, ray_depth, extras = self(ray_origs, ray_dirs)
 
-
-
-        # TODO: FIX ME
+        # Calculate losses for training
         proposal_loss = self.transmittance_estimator.compute_loss(extras["trans"])
         radiance_loss = nn.functional.mse_loss(ray_colors_pred, ray_colors)
 
+        # Calculate total loss
+        network_loss = proposal_loss + radiance_loss
+        
+        # Calculate PSNR
+        # NOTE: Cannot calculate SSIM because it relies on image patches
+        # NOTE: Cannot calculate LPIPS because it relies on image patches
+        psnr = -10 * th.log10(radiance_loss)
 
-
-
-
-
-        loss = proposal_loss + radiance_loss
-        # TODO: Log all the losses, including new psnr and stuff
-        
-        
-        
-        
-        
-        
-        
-        
-        self.log(f"{stage}_loss", loss)
+        # Log metrics
+        self.log_dict({
+            f"{stage}_network_loss": network_loss,
+            f"{stage}_proposal_loss": proposal_loss,
+            f"{stage}_radiance_loss": radiance_loss,
+            f"{stage}_psnr": psnr,
+        }) 
 
         # NOTE: Assuming losses are ordered according to associated optimizer
         return proposal_loss, radiance_loss
 
+
     def training_step(self, batch: DatasetOutput, batch_idx: int):
+        """Perform a single training forward pass and optimization step.
+        
+        Args:
+            batch (DatasetOutput): Batch of data
+            batch_idx (int): Index of the batch
+        
+        Returns:
+            th.Tensor: Total loss
+        """
         # Get optimizers for manual optimization
         # NOTE: Technically getting a list of LightningOptimizer wrappers,
         #  but they have not set up typing correctly. Expect more type ignores.
@@ -296,10 +293,19 @@ class Garf(pl.LightningModule):
 
         # Return summed loss
         return sum(losses)
-        
+
 
     def validation_step(self, batch: DatasetOutput, batch_idx: int):
-        return self._forward_loss(batch, batch_idx, "val")
+        """Perform a single validation forward pass.
+        
+        Args:
+            batch (DatasetOutput): Batch of data
+            batch_idx (int): Index of the batch
+        
+        Returns:
+            th.Tensor: Total loss
+        """
+        return sum(self._forward_loss(batch, batch_idx, "val"))
 
 
     def configure_optimizers(self):
