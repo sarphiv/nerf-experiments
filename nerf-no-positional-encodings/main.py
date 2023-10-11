2de18cd0892950ae86a5db16f09d38b3fb2e59f7
davidAlvaBro <davidalvabro@gmail.com>
2023-10-11 17:37:08 +0200

    final draft for test of transistion from vanilla to naive

--- nerf-no-positional-encodings/main.py
+++ nerf-no-positional-encodings/main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     wandb_logger = WandbLogger(
         project="nerf-experiments", 
         entity="metrics_logger",
-        name="normalized-vanilla-sanity-check"
+        name="nerf-no-positional-encodings"
     )
 
 
