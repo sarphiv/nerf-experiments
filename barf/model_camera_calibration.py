ffd97d6dacde4fb3defceb82f6ef0540cc444070
davidAlvaBro <davidalvabro@gmail.com>
2023-10-28 16:07:47 +0200

    WIP: barf wrapper complete - missing data_module and image_logger (maybe main?)

--- barf/model_camera_calibration.py
+++ barf/model_camera_calibration.py
@@ -12,7 +12,6 @@ MAGIC_NUMBER = 7
 
 
 class CameraExtrinsics(nn.Module):
-    #TODO: Instead of learning the identity, define a new rotation matrix that is applied before the learned one
     def __init__(self, noise: float, size: int) -> None:
         super().__init__()
         
@@ -61,58 +60,6 @@ class CameraExtrinsics(nn.Module):
         new_d = th.matmul(R, d.unsqueeze(-1)).squeeze(-1)
 
         return new_o, new_d, trans, R
-    
-    def get_training_rotation(self, indexes: th.Tensor, origins: th.Tensor) -> th.Tensor:
-        """
-        Finds the rotation matrix that minimizes the point clouds for origins and R origins. 
-
-        Parameters:
-        -----------
-            indexs: th.Tensor - the indexs all training images
-            origins: th.Tensor - the origins of the rays/images
-
-        Returns:
-        --------
-            T: th.Tensor - the translation vector
-            R: th.Tensor - the rotation matrix
-        """
-        # Extrinsics oriontation
-        R = self.get_rotations(indexes)
-
-        # Get the translations 
-        trans = self.params[indexes, 3:] 
-
-        # Get the predicted extrinsics origins
-        o = th.matmul(R, origins.unsqueeze(-1)).squeeze(-1) + trans 
-
-        def align_rotation(P, Q):
-            """
-            optimize ||P@R - Q||^2 using SVD
-            """
-            H = P.T@Q
-            U, S, V = th.linalg.svd(H)
-            d = th.linalg.det(V@U.T)
-            K = th.eye(len(S))
-            K[-1,-1] = d
-            R = U@K@V.T
-            return R
-
-        def align_paired_point_clouds(P, Q):
-            """
-            align paired point clouds P and Q
-            by translating and rotating P into Q
-            """
-            # translate P to origin
-            cP = th.mean(P, dim=0, keepdim=True)
-            cQ = th.mean(Q, dim=0, keepdim=True)
-            # rotate P to Q
-            R = align_rotation(P-cP, Q-cQ)
-            Qhat = (P - cP)@R + cQ
-            return Qhat, R, cQ-cP@R # t = cQ@R - cP
-
-        raise NotImplementedError("This function is not implemented yet")
-    
-    
 
 class NerfCameraCalibration(NerfInterpolation):
     def __init__(
@@ -125,67 +72,90 @@ class NerfCameraCalibration(NerfInterpolation):
         self.n_training_images = len(self.trainer.datamodule.dataset_train) # type: ignore
         self.camera_extrinsics = CameraExtrinsics(noise, self.n_training_images)
     
-    # def forward(self, ray_origs: th.Tensor, ray_dirs: th.Tensor, ray_index: th.Tensor) -> tuple[th.Tensor, th.Tensor]:
-    #     """
-    #     First apply the camera extrinsics, then the nerf as in vanilla
-    #     """
-    #     # Get the new origins and directions with the camera extrinsics module
-    #     ray_origs, ray_dirs, translations, rotations = self.camera_extrinsics(ray_index, ray_origs, ray_dirs)
-
-    #     # Run the vanilla nerf model 
-    #     return super().forward(ray_origs, ray_dirs)
     
-    def kabsc(self, raw_origs: th.Tensor, noise_origs: th.Tensor, indexes: th.Tensor) -> tuple[th.Tensor, th.Tensor]:
+    def kabsch_algorithm(self, point_cloud_1: th.Tensor, point_cloud_2: th.Tensor): 
         """
-        Find the best rotation and translation to map the given camera poses to the raw (true) ones. 
+        Align "point_cloud_2" to "point_cloud_1" with a matrix R and a vector t.
+        align_rotation: helper function that optimizes the subproblem ||P - Q@R||^2 using SVD
+        
+        Parameters:
+        -----------
+            point_cloud_1: th.Tensor - the point cloud where we transform to
+            point_cloud_2: th.Tensor - the point cloud where we transform from
+
+        Returns:
+        --------
+            R: th.Tensor - the rotation matrix
+            t: th.Tensor - the translation vector
         """
+        def align_rotation(P, Q):
+            """
+            Optimize ||P - Q@R||^2 using SVD
+            """
+            H = P.T@Q
+            U, S, V = th.linalg.svd(H) # In normal notation this is U, S, V^T 
+            d = th.linalg.det(V.T@U.T)
+            K = th.eye(len(S))
+            K[-1,-1] = d
+            R = V.T@K@U.T
+            return R
+
+        # Translate both point clouds to the origin 
+        mean_1 = th.mean(point_cloud_1, dim=0, keepdim=True)
+        mean_2 = th.mean(point_cloud_2, dim=0, keepdim=True)
+
+        # Find the rotation matrix such that ||(C1 - m1) - (C2 - m2)@R||^2 is minimized
+        R = align_rotation(point_cloud_1 - mean_1, point_cloud_2 - mean_2)
+
+        # The translation vector is now 
+        t = mean_1 - mean_2@R
+
+        return R, t
+    
+    def validation_transform(self, val_origs: th.Tensor, val_dirs: th.Tensor) -> tuple[th.Tensor, th.Tensor, th.Tensor, th.Tensor]:
+        """
+        Takes in validation rays and transforms them to the predicting space 
+
+        Parameters:
+        -----------
+            val_origs: th.Tensor - the origins of the rays
+            val_dirs: th.Tensor - the directions of the rays
+
+        Returns:
+        --------
+            new_origs: th.Tensor - the new origins of the rays
+            new_dirs: th.Tensor - the new directions of the rays
+            translations: th.Tensor - the translation vectors
+            rotations: th.Tensor - the rotation matrices
+        """
+        # Get the raw and noise origins 
+        raw_origs = self.trainer.datamodule.dataset_train.raw_origins # type: ignore
+        noise_origs = self.trainer.datamodule.dataset_train.noise_origins # type: ignore
+        indexes = th.arange(raw_origs.shape[0]) #TODO: check that this works and corresponds to [i[-1] for i in self.trainer.datamodule.dataset_train.dataset] ish 
+
+        # Get the predicted origins 
         pred_origs = self.camera_extrinsics(noise_origs, indexes)
 
-        return kabsc(pred_origs, raw_origs) -> R, T
-        raise NotImplementedError("This function is not implemented yet")
+        #TODO: add a flag so that we do not need to calculate the rotation matrix and translation vector every time if the network has not changed 
+        # Get the rotation matrix and the translation vector 
+        R, t = self.kabsch_algorithm(self, pred_origs, raw_origs) # type: ignore
+
+        # Transform the validation image to this space 
+        new_origs = th.matmul(R, val_origs.unsqueeze(-1)).squeeze(-1) + t
+        new_dirs = th.matmul(R, val_dirs.unsqueeze(-1)).squeeze(-1)
+
+        return new_origs, new_dirs, t, R
     
-    def validation_transform() -> ray_origs, ray_dirs
-        self.trainer.datamodule._get_dataset("train")
-        self.trainer.datamodule.dataset_train.raw_origins
-        self.trainer.datamodule.dataset_train.noise_origins
-        self.trainer.datamodule.train_dataloader()
-        raise NotImplementedError()
-    
-
-    # def validation_suck_ass(self, ray_origs: th.Tensor, ray_dirs: th.Tensor, ray_index: th.Tensor) -> tuple[th.Tensor, th.Tensor]:
-    #     """
-    #     Forward pass of the model for a validation step. 
-    #     When we are setting the camera extrinsics, the validation rays must be taken to the correct space.
-    #     """
-    #     #TODO: add a flag so that we do not need to calculate the rotation matrix and translation vector every time if the network has not changed 
-    #     # Calculate the rotation matrix and translation vector
-    #     translation, rotation = self.camera_extrinsics.get_training_rotation(ray_index, ray_origs, ray_dirs)
-
-    #     # Transform the validation image to this space 
-    #     new_origs = th.matmul(rotation, ray_origs.unsqueeze(-1)).squeeze(-1) + translation
-    #     new_dirs = th.matmul(rotation, ray_dirs.unsqueeze(-1)).squeeze(-1)
-
-    #     # Run the vanilla nerf model 
-    #     return super().forward(new_origs, new_dirs)
     
     ############ pytorch lightning functions ############
 
-    def _step_helpher(self, batch: DatasetOutput, stage: str):
+    def _step_helpher(self, ray_origs, ray_dirs, ray_colors, stage: str):
         """
         general function for training and validation step
         """
-        # TODO: Move these two lines to the respective train and val 
-        # Get the rays to sample from
-        ray_origs, ray_dirs, ray_colors, ray_index = batch
-        # TODO: Add schedular here that changes which gaussian to use
-        ray_colors = ray_colors[:, :, 0]
+        # Call the forward pass 
+        ray_colors_pred_coarse, ray_colors_pred_fine = self.forward(ray_origs, ray_dirs)
 
-        # Run forwards to get color predictions
-        if stage == "train":
-            ray_colors_pred_fine, ray_colors_pred_coarse = self(ray_origs, ray_dirs, ray_index)
-        else: 
-            ray_colors_pred_fine, ray_colors_pred_coarse = self.validation_forward(ray_origs, ray_dirs, ray_index)
-        
         # Compute the individual losses
         proposal_loss = nn.functional.mse_loss(ray_colors_pred_coarse, ray_colors)
         radiance_loss = nn.functional.mse_loss(ray_colors_pred_fine, ray_colors)
@@ -211,13 +181,28 @@ class NerfCameraCalibration(NerfInterpolation):
 
 
     def training_step(self, batch: DatasetOutput, batch_idx: int):
-        # TODO: Do this here (fix later)
+        # Get the rays to sample from
+        ray_origs, ray_dirs, ray_colors, ray_index = batch
+
+        # Transform to the prediction space 
         ray_origs, ray_dirs, translations, rotations = self.camera_extrinsics(ray_index, ray_origs, ray_dirs)
-        return self._step_helpher(batch, "train")
+
+        # TODO: Add schedular here that changes which gaussian to use (for now -1 one should be the last)
+        ray_colors = ray_colors[:, :, -1]
+
+        return self._step_helpher(ray_origs, ray_dirs, ray_colors, "train")
 
     def validation_step(self, batch: DatasetOutput, batch_idx: int):
-        # TODO: make this later (validation transform) 
-        return self._step_helpher(batch, "val")
+        # Get the rays to sample from
+        ray_origs, ray_dirs, ray_colors, _ = batch
+
+        # Transform to the predicted coordinates 
+        ray_origs, ray_dirs, translations, rotations = self.validation_transform(ray_origs, ray_dirs)
+
+        # Here we only care about the ray that has no noise applied to it, hence the -1
+        ray_colors = ray_colors[:, :, -1]
+
+        return self._step_helpher(ray_origs, ray_dirs, ray_colors, "train")
 
 
     def configure_optimizers(self):
