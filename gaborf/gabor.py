Gradient bug fixes and hypers changes

gaborf/gabor.py
@@ -18,17 +18,13 @@ class GaborActivation(th.autograd.Function):
     @staticmethod
     def backward(ctx: FunctionCtx, grad_output: th.Tensor):
         # Retrieve parameters
-        x, v, s = cast(tuple[th.Tensor, th.Tensor], ctx.saved_tensors) # type: ignore
+        x, v, s = cast(tuple[th.Tensor, th.Tensor, th.Tensor], ctx.saved_tensors) # type: ignore
 
         # Compute gradients
-        x2 = x**2
-        go_mevx2 = -th.exp(-v*x2) * grad_output
-        sinsx = th.sin(s*x)
-        cossx = th.cos(s*x)
-        
-        grad_input_x = go_mevx2 * (2*cossx*v*x + s*sinsx)
-        grad_input_v = go_mevx2 * x2 * cossx
-        grad_input_s = go_mevx2 * x * sinsx
+        go_mevx2 = -th.exp(-v*x**2) * grad_output
+        grad_input_x = go_mevx2 * (2*th.cos(s*x)*v*x + s*th.sin(s*x))
+        grad_input_v = go_mevx2 * x**2 * th.cos(s*x)
+        grad_input_s = go_mevx2 * x * th.sin(s*x)
 
         return grad_input_x, grad_input_v, grad_input_s
 
@@ -60,7 +56,7 @@ class GaborAct(nn.Module):
             th.rand(features_in) * (inv_standard_deviation_init_max - inv_standard_deviation_init_min) + inv_standard_deviation_init_min
         )
         self.spread = nn.Parameter(
-            th.rand(features_in) * 8 * th.pi
+            th.rand(features_in) * 2 * th.pi
         )
 
 

gaborf/model_proposal.py
@@ -16,15 +16,15 @@ class ProposalNetwork(nn.Module):
         self.gaussian_init_max = gaussian_init_max
         
         self._parameters_linear: list[nn.Parameter] = []
-        self._parameters_gaussian: list[nn.Parameter] = []
+        self._parameters_gabor: list[nn.Parameter] = []
 
         self.model = nn.Sequential(
             self._create_linear(3, 512),
-            self._create_gaussian(512),
+            self._create_gabor(512),
             self._create_linear(512, 256),
-            self._create_gaussian(256),
+            self._create_gabor(256),
             self._create_linear(256, 128),
-            self._create_gaussian(128),
+            self._create_gabor(128),
             self._create_linear(128, 1),
             nn.Softplus(threshold=8)
         )
@@ -37,9 +37,10 @@ class ProposalNetwork(nn.Module):
 
         return linear
 
-    def _create_gaussian(self, features_in) -> GaborAct:
+    def _create_gabor(self, features_in) -> GaborAct:
         act = GaborAct(features_in, self.gaussian_init_min, self.gaussian_init_max)
-        self._parameters_gaussian.append(act.inv_standard_deviation)
+        self._parameters_gabor.append(act.inv_standard_deviation)
+        self._parameters_gabor.append(act.spread)
 
         return act
 
@@ -47,8 +48,8 @@ class ProposalNetwork(nn.Module):
     def parameters_linear(self) -> Iterator[nn.Parameter]:
         return iter(self._parameters_linear)
 
-    def parameters_gaussian(self) -> Iterator[nn.Parameter]:
-        return iter(self._parameters_gaussian)
+    def parameters_gabor(self) -> Iterator[nn.Parameter]:
+        return iter(self._parameters_gabor)
 
 
     def forward(self, pos: th.Tensor) -> tuple[th.Tensor, th.Tensor]:

gaborf/model_radiance.py
@@ -16,29 +16,29 @@ class RadianceNetwork(nn.Module):
         self.gaussian_init_max = gaussian_init_max
         
         self._parameters_linear: list[nn.Parameter] = []
-        self._parameters_gaussian: list[nn.Parameter] = []
+        self._parameters_gabor: list[nn.Parameter] = []
 
 
         # Creates the first module of the network 
         self.model_density_1 = nn.Sequential(
             self._create_linear(3, 1024),
-            self._create_gaussian(1024),
+            self._create_gabor(1024),
             self._create_linear(1024, 256),
-            self._create_gaussian(256),
+            self._create_gabor(256),
             self._create_linear(256, 128),
-            self._create_gaussian(128),
+            self._create_gabor(128),
             self._create_linear(128, 128),
-            self._create_gaussian(128),
+            self._create_gabor(128),
         )
 
         # Creates the second module of the network (skip connection of input to the network again)
         self.model_density_2 = nn.Sequential(
             self._create_linear(128 + 3, 512),
-            self._create_gaussian(512),
+            self._create_gabor(512),
             self._create_linear(512, 256),
-            self._create_gaussian(256),
+            self._create_gabor(256),
             self._create_linear(256, 128),
-            self._create_gaussian(128),
+            self._create_gabor(128),
             self._create_linear(128, 128 + 1)
         )
 
@@ -48,7 +48,7 @@ class RadianceNetwork(nn.Module):
         # Creates the final module of the model that outputs the color
         self.model_color = nn.Sequential(
             self._create_linear(128 + 3, 256),
-            self._create_gaussian(256),
+            self._create_gabor(256),
             self._create_linear(256, 3),
             nn.Sigmoid()
         )
@@ -61,9 +61,10 @@ class RadianceNetwork(nn.Module):
 
         return linear
 
-    def _create_gaussian(self, features_in) -> GaborAct:
+    def _create_gabor(self, features_in) -> GaborAct:
         act = GaborAct(features_in, self.gaussian_init_min, self.gaussian_init_max)
-        self._parameters_gaussian.append(act.inv_standard_deviation)
+        self._parameters_gabor.append(act.inv_standard_deviation)
+        self._parameters_gabor.append(act.spread)
 
         return act
 
@@ -71,8 +72,8 @@ class RadianceNetwork(nn.Module):
     def parameters_linear(self) -> Iterator[nn.Parameter]:
         return iter(self._parameters_linear)
 
-    def parameters_gaussian(self) -> Iterator[nn.Parameter]:
-        return iter(self._parameters_gaussian)
+    def parameters_gabor(self) -> Iterator[nn.Parameter]:
+        return iter(self._parameters_gabor)
 
 
     def forward(self, pos: th.Tensor, dir: th.Tensor) -> tuple[th.Tensor, th.Tensor]: