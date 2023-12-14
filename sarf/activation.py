0dca6dc52329615fbbb1f1ab0e32effd81a7357e
sarphiv <32750711+sarphiv@users.noreply.github.com>
2023-12-14 06:25:33 +0100

    More aggressive NaN mitigation

--- sarf/activation.py
+++ sarf/activation.py
@@ -11,7 +11,7 @@ class SarfActivation(th.autograd.Function):
     def forward(ctx: FunctionCtx, x: th.Tensor, f: th.Tensor):
         # Avoid division by zero
         # NOTE: Approaches f for x = 0, so small numbers should be fine
-        x = th.sign(x) * (th.abs(x) + th.finfo(x.dtype).tiny)
+        x = th.sign(x) * (th.abs(x) + 1e-4)
 
         # Save parameters
         ctx.save_for_backward(x, f)
@@ -26,10 +26,14 @@ class SarfActivation(th.autograd.Function):
         # NOTE: Forward pass ensures x != 0
         x, f = cast(tuple[th.Tensor, th.Tensor], ctx.saved_tensors) # type: ignore
 
+        # Cache
+        fx = f*x
+        cos = th.cos(fx)
+
         # Return gradients
         return (
-            grad_output * (f*th.cos(f*x)*x - th.sin(f*x)) / x**2,
-            grad_output * th.cos(f*x)
+            grad_output * (f*cos*x - th.sin(fx)) / x**2,
+            grad_output * cos
         )
 
 
@@ -37,8 +41,8 @@ class SarfAct(nn.Module):
     def __init__(
             self,
             features_in: int,
-            frequency_init_min: float = 0.,
-            frequency_init_max: float = 1.
+            frequency_init_min: float,
+            frequency_init_max: float
         ):
             """
             Initialize the SARF activation function.
